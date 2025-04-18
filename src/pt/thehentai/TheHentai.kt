ee3880e1aef8a0972147e5c8bd55fe99406c3a9e
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-18 10:24:10 -0300

    Delete src/pt/thehentai/TheHentai.kt

--- src/pt/thehentai/TheHentai.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.thehentai
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class TheHentai : Madara(
-    "The Hentai",
-    "https://thehentai.net",
-    "pt-BR"
-)
