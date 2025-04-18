31eeeaac05a78540986fc0afe1d9d33d23537f27
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-18 09:35:19 -0300

    Add files via upload

---
+++ src/pt/thehentai/TheHentai.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.pt.thehentai
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class TheHentai : Madara(
+    "The Hentai",
+    "https://thehentai.net",
+    "pt-BR"
+)
