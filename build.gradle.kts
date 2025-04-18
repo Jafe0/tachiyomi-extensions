49d1e82b0044559278a2690f4df7754af26a591c
Jafe0 <expeditoromulo1555@gmail.com>
2025-04-18 10:38:48 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,30 @@
 plugins {
-    id("com.android.library") version "8.2.2" apply false
-    kotlin("android") version "1.9.22" apply false
+    id("com.android.library")
+    kotlin("android")
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("eu.kanade.tachiyomi:source-api:1.5")
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 }
